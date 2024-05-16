1a5fc6cd2623784572b4a7547e1d20fdda76c7c4
ravel01106 <amandaravelocabrera@gmail.com>
2024-05-16 10:53:36 +0100

    feat: added contact component in contactContainer component

--- components/contactscontainer/ContactsContainer.tsx
+++ components/contactscontainer/ContactsContainer.tsx
@@ -1,52 +1,25 @@
-import {
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ImageProps,
-} from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import React from "react";
 import { allUsersContext } from "../../context/AllUsersContext";
 import { IUsersResponse } from "../../interfaces/users/allusers/IUsers";
-import { IGuestUser } from "../../interfaces/users/guestuser/IGuestUser";
-import { NavigationContext } from "@react-navigation/native";
-import { currentGuestUserContext } from "../../context/CurrentGuestUserContetxt";
+import Contact from "../contact/Contact";
 
 const ContactsContainer = () => {
-  const navigation = React.useContext(NavigationContext);
-  const { setGuestUser } = React.useContext(currentGuestUserContext);
-  const navigateChat = (id: String, name: String) => {
-    const data: IGuestUser = {
-      id: id.toString(),
-      name: name.toString(),
-    };
-    setGuestUser(data);
-    navigation?.navigate("Chat");
-  };
   const { users } = React.useContext(allUsersContext);
-  const avatarDefault: ImageProps = require("../../assets/images/avatarPredefinido.png");
   return (
     <ScrollView style={styles.ScrollContainer}>
       {users.map((user: IUsersResponse) => (
-        <Pressable
+        <Contact
+          _id={user._id}
+          name={user.name}
+          lastName={user.lastName}
+          email={user.email}
+          department={user.department}
+          isActive={user.isActive}
+          password={user.password}
+          idRole={user.idRole}
           key={user._id.toString()}
-          onPress={() => navigateChat(user._id, user.name)}
-        >
-          <View style={styles.contactContainer}>
-            <View style={styles.contactSubContainer}>
-              <View style={styles.avatarContainer}>
-                <Image source={avatarDefault} style={styles.avatarStyle} />
-              </View>
-              <View style={styles.textContainer}>
-                <Text style={styles.textStyle}>
-                  {user.name} {user.lastName}
-                </Text>
-              </View>
-            </View>
-          </View>
-        </Pressable>
+        />
       ))}
     </ScrollView>
   );
@@ -55,41 +28,8 @@ const ContactsContainer = () => {
 export default ContactsContainer;
 
 const styles = StyleSheet.create({
-  contactContainer: {
-    backgroundColor: "#D9D9D9",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    paddingHorizontal: 15,
-    paddingVertical: 8,
-  },
-  contactSubContainer: {
-    width: "80%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-  },
-  avatarContainer: {
-    paddingRight: 10,
-  },
-  lastMessageStyle: {
-    color: "#606060",
-    width: 200,
-  },
-  textStyle: {
-    color: "black",
-    fontSize: 16,
-    fontWeight: "500",
-  },
   ScrollContainer: {
     marginBottom: 25,
     height: "100%",
-  },
-  avatarStyle: {
-    height: 80,
-    width: 80,
-  },
-  textContainer: {
-    width: "60%",
   },
 });
